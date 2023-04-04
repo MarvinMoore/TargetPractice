65c5fe6be53b3ba2d22f5079b448060640330e36
Marvin Moore <91348941+MarvinMoore@users.noreply.github.com>
2023-04-04 12:06:32 -0500

    Main functionality
    added player, environment, bow, arrow mechanics

---
+++ TargetPractice/Assets/Bow.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bow : MonoBehaviour
+{
+    public GameObject arrow;
+    public float launchForce;
+    public Transform shotPoint;
+    public Slider forceUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector2 bowPosition = transform.position;
+       Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+       Vector2 direction = mousePosition - bowPosition;
+       transform.right = direction;
+
+       if (Input.GetMouseButton(0))
+        {
+            launchForce++;
+            slider();
+            if (launchForce > 100){
+                ResetGuage();
+            }
+        }
+        if (Input.GetMouseButtonUp(0)){
+            Shoot();
+            StartCoroutine(Wait());
+       }
+        void Shoot() {
+            GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);
+            print(launchForce);
+            newArrow.GetComponent<Rigidbody2D>().velocity = transform.right * (launchForce/4);
+        }
+    }
+    public void slider()
+    {
+        forceUI.value = launchForce;
+    }
+    public void ResetGuage()
+    {
+        launchForce = 0;
+        forceUI.value = 0;
+    }
+    IEnumerator Wait(){
+        yield return new WaitForSeconds(1.5f); //Wait a while before resetting slider value/force
+        ResetGuage(); //Reset slider/force after shooting
+    }
+}
